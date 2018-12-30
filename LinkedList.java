09222159900f575a2e6e468cfbed09825f0efcc1
Sarvesh Singh <007sarveshsingh@gmail.com>
2018-12-30 17:45:00 +0530

    SwapNode of linkedlist
    Creating a linkedlist

--- LinkedList.java
+++ LinkedList.java
@@ -1,149 +1,70 @@
-import java.util.*;
-class Node{
-    int data;
-    Node next;
+import java.util.Scanner;
+class LinkedListNode{
+	int data;
+	LinkedListNode next;
 }
 public class LinkedList{
-    Node head=null;
-    Node prev=null;
-    Node prev1 = null;
-    public void insert(int data){
-        Node node =  new Node();
-        if(head==null){
-            node.data = data;
-            node.next = null;
-            head = node;
-            prev = head;
-        }
-    }
-    public void insertAtEnd(int data){
-        Node endNode =  new Node();
-        endNode.data = data;
-        prev.next = endNode;
-        endNode.next = null;
-        prev = endNode;
-    }
-    public void insertAtBegining(int data){
-        Node start =  new Node();
-        start.data = data;
-        start.next = head;
-        head = start;
-    }
-    public void deletePosition(int pos){
-        Node position = null;
-        Node prev = null;
-        Node temp = head;
-        int c = 0;
-        if(pos<1)
-        return;
-        while(temp!=null){
-            c++;
-            if(c==pos){
-                position = temp;
-                break;
-            }
-            else{
-                prev = temp;
-            }
-            temp = temp.next;
-        }
-        if(pos>c)
-            return;
-        else if(pos == 1){
-            head = position.next;
-        }
-        else{
-           prev.next = position.next;
-        }
-    }
-    public void insertAtPosition(int data, int pos){
-        Node position = null;
-        Node newNode = new Node();
-        Node temp = head;
-        int c = 0;
-        if(pos<1)
-        return;
-        while(temp!=null){
-            c++;
-            if(c==pos){
-                position = temp;
-                break;
-            }
-            else{
-                prev1 = temp;
-            }
-            temp = temp.next;
-        }
-        if(pos>c+1)
-        return;
-        else if(pos == 1){
-            newNode.data = data;
-            newNode.next = head;
-            head = newNode;
-        }
-        else{
-            newNode.data = data;
-            newNode.next = position;
-            prev1.next = newNode;
-        }
-    }
-    public void disp(){
-        Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.data+" ");
-            temp = temp.next;
-        }
-        System.out.println();
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        LinkedList linkedList = new LinkedList();
-        System.out.println("Enter Data for first Node");
-        int data = sc.nextInt();
-        linkedList.insert(data);
-        while(true){
-			System.out.println("1 InsertAtBegining\t2 InsertAtEnd\t3 InsertAtPosition\t4 deleteAtPosition\t5 display\t6 Exit");
-			int ch = sc.nextInt();
-			switch(ch){
-				case 1:
-					int newdata = sc.nextInt(); 
-					linkedList.insertAtBegining(newdata);
-					break;
-				case 2:
-					int newdata1 = sc.nextInt(); 
-					linkedList.insertAtEnd(newdata1);
-					break;
-				case 3:
-					int newdata2 = sc.nextInt();
-					int posi = sc.nextInt(); 
-					linkedList.insertAtPosition(newdata2,posi);
-					break;
-				case 4:
-					int pos = sc.nextInt(); 
-					linkedList.deletePosition(pos);
-					break;
-				case 5:
-					linkedList.disp();
-					break;
-				case 6:
-					System.exit(0);
-					break;
-				default:
-					System.out.println("Wrong choice");
-					break;
-			}
+	LinkedListNode root;
+	LinkedListNode prev;
+	public void insert(int data) {
+		LinkedListNode node  = new LinkedListNode();
+		node.data = data;
+		node.next = null;
+		if(root == null) {
+			root  = node;
+			prev = node;
 		}
-       /* int data = sc.nextInt();
-        linkedList.insert(data);
-        linkedList.insertAtEnd(3);
-        linkedList.insertAtBegining(5);
-        linkedList.insertAtEnd(6);
-        linkedList.insertAtBegining(10);
-        linkedList.insertAtPosition(0,5);
-        linkedList.insertAtPosition(21,4);
-        linkedList.deletePosition(1);
-        linkedList.disp();
-        linkedList.deletePosition(6);
-        linkedList.disp();*/
-    }
+		else {
+			prev.next = node;
+			prev = node;
+		}
+	}
+	public void swapNode(int x, int y) {
+		if (x == y) return; 
+        LinkedListNode a = null, temp1 = root; 
+        while (temp1 != null && temp1.data != x) 
+        { 
+            a = temp1; 
+            temp1 = temp1.next; 
+        }
+        LinkedListNode b = null, temp2 = root; 
+        while (temp2 != null && temp2.data != y) 
+        { 
+            b = temp2;
+            temp2 = temp2.next; 
+        } 
+        if (temp1 == null || temp2 == null) 
+            return; 
+        if (temp1 != null) 
+            a.next = temp2; 
+        else 
+            root = temp2; 
+        if (temp2 != null) 
+            b.next = temp1; 
+        else 
+            root = temp1; 
+        LinkedListNode temp = temp1.next; 
+        temp1.next = temp2.next; 
+        temp2.next = temp; 
+	}
+	public void disp() {
+		LinkedListNode cur = root;
+		while(cur!=null) {
+			System.out.println(cur.data);
+			cur = cur.next;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		int n = sc.nextInt();
+		LinkedList linkedList  = new LinkedList();
+		for(int i =0;i<n;i++) {
+			int data  = sc.nextInt();
+			linkedList.insert(data);
+		}
+		linkedList.disp();
+		linkedList.swapNode(2,4);
+		linkedList.disp();
+		sc.close();
+	}
 }
